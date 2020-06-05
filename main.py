086cd506624cb70210ab8c704d7507f4165bc0f1
ccc814p <63892230+ccc814p@users.noreply.github.com>
2020-06-05 00:59:02 -0400

    Exit
    Allows user to exit Python-DOS

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ while True:
     g.close()
   if cmd[0] == 'makedir':
     os.mkdir(cmd[1])
-  if not cmd[0] in ('start', 'echo', 'dir', 'del', 'open', 'makedir', 'cd', 'ddir', 'help', 'clone', 'rename', 'java', 'move', 'cls', 'python3'):
+  if not cmd[0] in ('start', 'echo', 'dir', 'del', 'open', 'makedir', 'cd', 'ddir', 'help', 'clone', 'rename', 'java', 'move', 'cls', 'python3', 'time', 'date', 'setDate'):
     print("Bad command")
   if cmd[0] == 'cd':
     os.chdir(cmd[1])
@@ -33,7 +33,7 @@ while True:
   if cmd[0] == 'java':
     os.system(f"java {cmd[1]}")
   if cmd[0] == 'help':
-    print("\nstart: starts a program)\necho: echos the following text\ndir: shows all files in a directory\ndel: deletes a file\nopen: shows contents of a file\nmakedir: creates the following directory inside the current directory\ncd: changes directory to the following dir\nddir: deletes the following dir\nrename: renames a file or directory\nclone: clones a file or directory to a certain dir\njava: runs a java program\nmove: moves a file to a requested directory\npython3: starts a python 3 program\ndate: shows current date\ntime: shows current time\nsetDate: sets date to the following. Format is 'year, month, day'\n")
+    print("\nstart: starts a program)\necho: echos the following text\ndir: shows all files in a directory\ndel: deletes a file\nopen: shows contents of a file\nmakedir: creates the following directory inside the current directory\ncd: changes directory to the following dir\nddir: deletes the following dir\nrename: renames a file or directory\nclone: clones a file or directory to a certain dir\njava: runs a java program\nmove: moves a file to a requested directory\npython3: starts a python 3 program\ndate: shows current date\ntime: shows current time\nsetDate: sets date to the following. Format is 'year, month, day'\nexit: exits Py-DOS")
   if cmd[0] == 'rename':
     os.rename(cmd[1], cmd[2])
   if cmd[0] == 'clone':
@@ -49,3 +49,5 @@ while True:
     print(time)
   if cmd[0] == 'setDate':
     today = datetime(int(cmd[1]), int(cmd[2]), int(cmd[3]))
+  if cmd[0] == 'exit':
+    os.system(exit)
